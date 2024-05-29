b89fd9cacdb4f944f5e0e684c4e5febc636d124f
Emre Sunar <134863869+emrxv@users.noreply.github.com>
2024-05-29 15:37:20 -0400

    back2

--- mcu_utils/common.mk
+++ mcu_utils/common.mk
@@ -2,11 +2,11 @@ ifndef LLVM_ADIN_PATH
 $(error "LLVM_ADIN_PATH variable not set!")
 endif
 
-CLANG           = clang
-CLANGPP         = clang++
-AR              = llvm-ar
+CLANG			= clang
+CLANGPP 		= clang++
+AR 				= llvm-ar
 
-BUILD_DIR       = $(BUILD_DIRECTORY)
+BUILD_DIR 	= $(BUILD_DIRECTORY)
 
 OPT_FLAGS += -adin -S
 
@@ -19,19 +19,21 @@ CBE = echo
 
 IR_SUFFIX = ll
 
-OPT         = "$(LLVM_ADIN_PATH)/opt"
-LD          = "$(LLVM_ADIN_PATH)/llvm-link"
+OPT			= "$(LLVM_ADIN_PATH)/opt"
+LD 			= "$(LLVM_ADIN_PATH)/llvm-link"
 
 all: clean_build $(BUILD_DIR) $(OUTPUT)
 
+
 .PHONY: clean_build
 clean_build:
-    rm -rf $(BUILD_DIR)
+	rm -rf $(BUILD_DIR)
 
 $(BUILD_DIR):
-    mkdir -p $@
+	mkdir -p $@
 
 print-%  : ; @echo $* = $($*)
+
 
 INCLUDE_LOCAL  += $(foreach INC,$(addprefix $(PRJ_PATH),$(INC_PATH)),-I$(INC))
 
@@ -41,31 +43,24 @@ IR_FILES += $(C_SRC:.c=.ll)
 IR_FILES += $(CPP_SRC:.cpp=.ll)
 
 $(OUTPUT): $(IR_FILES)
-    $(LD) -v $(BUILD_DIR)/*.adin.ll -S -o $@.ll
-    $(CLANG) $(COMPILE_FLAGS) -c $@.ll -o $@
-    #$(AR) rcv $@.ar $(BUILD_DIR)/*.adin.o
+	$(LD) -v $(BUILD_DIR)/*.adin.ll -S -o $@.ll
+	$(CLANG) $(COMPILE_FLAGS) -c $@.ll -o $@
+	#$(AR) rcv $@.ar $(BUILD_DIR)/*.adin.o
 
 REMCU_C: $(IR_FILES)
-    @echo done
+	@echo done
 
-# Compile
+
+# compile
 #------------------------------------------------------------------------------- 
 %.ll: %.c
-    if [ -f $< ]; then \
-        echo "Compiling $< to LLVM IR"; \
-        $(CLANG) $(IR_FLAGS) $(LOCAL_CFLAGS) -c $< -o $(BUILD_DIR)/$(notdir $<).$(IR_SUFFIX) --verbose; \
-        echo "Optimizing $(BUILD_DIR)/$(notdir $<).ll"; \
-        $(OPT) $(OPT_FLAGS) $(BUILD_DIR)/$(notdir $<).ll -o $(BUILD_DIR)/$(notdir $<).adin.ll --debug; \
-    else \
-        echo "File $< not found!"; \
-    fi
+	$(CLANG) $(IR_FLAGS) $(LOCAL_CFLAGS) -c $< -o $(BUILD_DIR)/$(notdir $<).$(IR_SUFFIX)
+	$(OPT) $(OPT_FLAGS) $(BUILD_DIR)/$(notdir $<).ll -o  $(BUILD_DIR)/$(notdir $<).adin.ll
+# 	$(CBE) $(CBE_OPTIONS) $(BUILD_DIR)/$(notdir $<).adin.ll -o $(OUTPUT_DIR)/$(notdir $<).remcu.c
+# 	$(CBE) $(CBE_OPTIONS) $(BUILD_DIR)/$(notdir $<).$(IR_SUFFIX) -o $(OUTPUT_MCU_DIR)/$(notdir $<).mcu.c
+
+	#$(CLANG) $(COMPILE_FLAGS) -c $(BUILD_DIR)/$(notdir $<).adin.ll -o $(BUILD_DIR)/$(notdir $<).adin.o
 
 %.ll: %.cpp
-    if [ -f $< ]; then \
-        echo "Compiling $< to LLVM IR"; \
-        $(CLANGPP) $(IR_FLAGS) $(LOCAL_CFLAGS) -c $< -o $(BUILD_DIR)/$(notdir $<).$(IR_SUFFIX) --verbose; \
-        echo "Optimizing $(BUILD_DIR)/$(notdir $<).ll"; \
-        $(OPT) $(OPT_FLAGS) $(BUILD_DIR)/$(notdir $<).ll -o $(BUILD_DIR)/$(notdir $<).adin.ll --debug; \
-    else \
-        echo "File $< not found!"; \
-    fi
+	$(CLANGPP) $(IR_FLAGS) $(LOCAL_CFLAGS) -c $< -o $(BUILD_DIR)/$(notdir $<).$(IR_SUFFIX)
+	$(OPT) $(OPT_FLAGS) $(BUILD_DIR)/$(notdir $<).ll -o  $(BUILD_DIR)/$(notdir $<).adin.ll
