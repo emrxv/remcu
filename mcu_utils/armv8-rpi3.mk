9821ab0f33c5f9d4de6aa10bcf040f9852491119
Emre Sunar <134863869+emrxv@users.noreply.github.com>
2024-05-29 16:32:51 -0400

    Update armv8-rpi3.mk

--- mcu_utils/armv8-rpi3.mk
+++ mcu_utils/armv8-rpi3.mk
@@ -1,2 +1,2 @@
-COMPILE_FLAGS = -fPIC --target=arm-linux-gnueabihf --gcc-toolchain=$(RASPBERRY_TOOL_PATH)/x-tools/armv8-rpi3-linux-gnueabihf/bin/
+COMPILE_FLAGS = -fPIC -mcpu=cortex-a53 -mfpu=neon-fp-armv8 --target=arm-linux-gnueabihf --gcc-toolchain=$(RASPBERRY_TOOL_PATH)/x-tools/armv8-rpi3-linux-gnueabihf/bin/
 IR_FLAGS += --target=arm-linux-gnueabihf
