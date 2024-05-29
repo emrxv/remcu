f5dfcc20511abb2f1c2cad212aa95c3df39c06aa
Emre Sunar <134863869+emrxv@users.noreply.github.com>
2024-05-29 15:51:58 -0400

    - debug

--- mcu_utils/common.mk
+++ mcu_utils/common.mk
@@ -56,7 +56,7 @@ REMCU_C: $(IR_FILES)
 %.ll: %.c
 	$(CLANG) $(IR_FLAGS) $(LOCAL_CFLAGS) -c $< -o $(BUILD_DIR)/$(notdir $<).$(IR_SUFFIX) --verbose
 	echo "Running OPT on $(BUILD_DIR)/$(notdir $<).ll"
-	$(OPT) $(OPT_FLAGS) $(BUILD_DIR)/$(notdir $<).ll -o  $(BUILD_DIR)/$(notdir $<).adin.ll --debug
+	$(OPT) $(OPT_FLAGS) $(BUILD_DIR)/$(notdir $<).ll -o  $(BUILD_DIR)/$(notdir $<).adin.ll
 # 	$(CBE) $(CBE_OPTIONS) $(BUILD_DIR)/$(notdir $<).adin.ll -o $(OUTPUT_DIR)/$(notdir $<).remcu.c
 # 	$(CBE) $(CBE_OPTIONS) $(BUILD_DIR)/$(notdir $<).$(IR_SUFFIX) -o $(OUTPUT_MCU_DIR)/$(notdir $<).mcu.c
 
@@ -65,4 +65,4 @@ REMCU_C: $(IR_FILES)
 %.ll: %.cpp
 	$(CLANGPP) $(IR_FLAGS) $(LOCAL_CFLAGS) -c $< -o $(BUILD_DIR)/$(notdir $<).$(IR_SUFFIX) --verbose
 	echo "Running OPT on $(BUILD_DIR)/$(notdir $<).ll"
-	$(OPT) $(OPT_FLAGS) $(BUILD_DIR)/$(notdir $<).ll -o  $(BUILD_DIR)/$(notdir $<).adin.ll --debug
+	$(OPT) $(OPT_FLAGS) $(BUILD_DIR)/$(notdir $<).ll -o  $(BUILD_DIR)/$(notdir $<).adin.ll
