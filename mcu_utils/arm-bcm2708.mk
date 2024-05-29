2278961146886bb6d5f85e3e8c9766da67ccfab4
Emre Sunar <134863869+emrxv@users.noreply.github.com>
2024-05-29 12:17:59 -0400

    new mk file

--- mcu_utils/arm-bcm2708.mk
+++
@@ -1,2 +0,0 @@
-COMPILE_FLAGS = -fPIC --target=arm-linux-gnueabihf --gcc-toolchain=$(RASPBERRY_TOOL_PATH)/arm-bcm2708/arm-rpi-4.9.3-linux-gnueabihf/bin/
-IR_FLAGS += --target=arm-linux-gnueabihf
---
+++ mcu_utils/armv8-rpi3.mk
@@ -0,0 +1,2 @@
+COMPILE_FLAGS = -fPIC --target=arm-linux-gnueabihf --gcc-toolchain=$(RASPBERRY_TOOL_PATH)/x-tools/armv8-rpi3-linux-gnueabihf/bin/
+IR_FLAGS += --target=arm-linux-gnueabihf
