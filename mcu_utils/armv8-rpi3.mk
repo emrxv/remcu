802b11058efc75e1139a399e28c5ec4765b6cb56
Emre Sunar <134863869+emrxv@users.noreply.github.com>
2024-07-19 16:12:17 -0400

    a53

--- mcu_utils/armv8-rpi3.mk
+++ mcu_utils/armv8-rpi3.mk
@@ -6,8 +6,8 @@
 CMSIS_PATH = $(BUILD_DIRECTORY)/stm32/stm32f10X/STM32F10x_StdPeriph_Lib_V3.5.0/Libraries/CMSIS/Core/Include
 HAL_PATH = $(BUILD_DIRECTORY)/stm32/stm32f10X/STM32F10x_StdPeriph_Lib_V3.5.0/Libraries/STM32F10x_StdPeriph_Driver/inc
 
-# Compiler flags for Cortex-M4
-COMPILE_FLAGS = -fPIC -mcpu=cortex-m4 -mthumb -O2 -Wall -I$(CMSIS_PATH) -I$(HAL_PATH) --target=arm-linux-gnueabihf --gcc-toolchain=$(RASPBERRY_TOOL_PATH)/x-tools/armv8-rpi3-linux-gnueabihf/bin/
+# Compiler flags for Cortex-A53
+COMPILE_FLAGS = -fPIC -mcpu=cortex-a53 -mthumb -O2 -Wall -I$(CMSIS_PATH) -I$(HAL_PATH) --target=arm-linux-gnueabihf --gcc-toolchain=$(RASPBERRY_TOOL_PATH)/x-tools/armv8-rpi3-linux-gnueabihf/bin/
 
 # Include CMSIS and HAL paths in IR_FLAGS
 IR_FLAGS += -I $(CMSIS_PATH) -I $(HAL_PATH) --target=arm-linux-gnueabihf
